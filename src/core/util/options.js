5a2c3b6d6e6ac4bca9afa09f5b89dcd1af875ee5
songlairui <cnre@live.cn>
2018-12-31 02:06:26 +0800

    vue 2.5 版本inject api

--- src/core/util/options.js
+++ src/core/util/options.js
@@ -267,15 +267,29 @@ function normalizeProps (options: Object) {
 }
 
 /**
- * Normalize all injections into Object-based format
+ * Normalize all injections into Object-based format (vue 2.5)
  */
-function normalizeInject (options: Object) {
+function normalizeInject (options: Object, vm: ?Component) {
   const inject = options.inject
+  if (!inject) return
+  const normalized = options.inject = {}
   if (Array.isArray(inject)) {
-    const normalized = options.inject = {}
     for (let i = 0; i < inject.length; i++) {
-      normalized[inject[i]] = inject[i]
+      normalized[inject[i]] = { from: inject[i] }
     }
+  } else if (isPlainObject(inject)) {
+    for (const key in inject) {
+      const val = inject[key]
+      normalized[key] = isPlainObject(val)
+        ? extend({ from: key }, val)
+        : { from: val }
+    }
+  } else if (process.env.NODE_ENV !== 'production') {
+    warn(
+      `Invalid value for option "inject": expected an Array or an Object, ` +
+      `but got ${inject}.`,
+      vm
+    )
   }
 }
 
