aee578eb1cf43a10e605e6ab90b8a7ef3aaad869
xutao15 <xutao15@meituan.com>
2019-07-31 21:17:30 +0800

    diff data

--- src/platforms/mp/util/index.js
+++ src/platforms/mp/util/index.js
@@ -62,3 +62,43 @@ export const eventTypeMap = {
   scrolltolower: ['scrolltolower'],
   scroll: ['scroll']
 }
+
+// vm上的数据深拷贝
+function isObject (obj) {
+  return (typeof obj === 'object' || typeof obj === 'function') && obj !== null
+}
+
+export function cloneDeep (data, hash = new WeakMap()) {
+  if (!isObject(data) || !data || !data.constructor) {
+    return data
+  }
+  let copyData
+  const Constructor = data.constructor
+  // 实际情况中，正则表达式会被以{}存储，Date对象会以时间字符串形式存储
+  // 函数则变为null
+  switch (Constructor) {
+    case RegExp:
+      copyData = new Constructor(data)
+      break
+    case Date:
+      copyData = new Constructor(data.getTime())
+      break
+    default:
+      // 循环引用问题解决
+      if (hash.has(data)) {
+        return hash.get(data)
+      }
+      copyData = new Constructor()
+      hash.set(data, copyData)
+  }
+  const symbols = Object.getOwnPropertySymbols(data)
+  if (symbols && symbols.length) {
+    symbols.forEach(symkey => {
+      copyData[symkey] = isObject(data[symkey]) ? cloneDeep(data[symkey], hash) : data[symkey]
+    })
+  }
+  for (var key in data) {
+    copyData[key] = isObject(data[key]) ? cloneDeep(data[key], hash) : data[key]
+  }
+  return copyData
+}
